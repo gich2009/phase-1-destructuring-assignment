28279de4d4fd2c6435859867111ccc097599bbf7
gich2009 <waneneamos@gmail.com>
2023-03-25 03:01:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -28,21 +28,37 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+let [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const farmAnimalsArray = farmAnimals.split(" ");
+let copyOfFarmAnimals = [...farmAnimalsArray.slice(0, 1), ...farmAnimalsArray.slice(2)];
+let [bessie, dolly, babe, little] =  copyOfFarmAnimals;
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+copyOfFarmAnimals = [...copyOfFarmAnimals.slice(0, 3)];
+let [blackAndWhite, black, pink] =  copyOfFarmAnimals;
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+let [red, orange, yellow, green, blue, indigo, violet] = colors;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+let colorsCopy = [...colors.slice(0, 5), ...colors.slice(6)];
+let [r, o, y, g, b, v] = colorsCopy;
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+colorsCopy = [...colors.slice(5, 6)];
+let [indg] = colorsCopy;
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+let {muppetName, color, song, job, partner} = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+let copyNestedMuppet = {...nestedMuppet, album: {theMuppetMovie: {song2: "Moving Right Along", song4: "I Hope That Something Better Comes Along"}}}
+
+let {album:{theMuppetMovie: {song2, song4}}, nestedJob, nestedPartner} = nestedMuppet;
+
